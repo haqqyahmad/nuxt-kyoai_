1c43dbc67db0e853e39d1c52fe66cf417ce9bdf3
haqqyahmad <haqqyahmad11@gmail.com>
2026-05-13 13:46:06 +0700

    repair view Items

---
+++ app/pages/items.vue
@@ -0,0 +1,47 @@
+<!-- app/pages/items/index.vue -->
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const route = useRoute()
+const links: NavigationMenuItem[] = [
+  {
+    label: 'MCU',
+    icon: 'i-lucide-clipboard',
+    to: '/items'
+  }
+]
+console.log('path', route.path)
+
+// const linkHidden = ['/settings/roles', '/settings/roles/new']
+</script>
+
+<template>
+  <UDashboardPanel id="items" :ui="{ body: 'py-4 sm:py-6' }">
+    <template #header>
+      <UDashboardNavbar title="Items">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+      </UDashboardNavbar>
+
+      <!-- <UDashboardToolbar v-if="!route.path.includes('/settings/roles')"> -->
+      <UDashboardToolbar>
+        <!-- NOTE: The `-mx-1` class is used to align with the `DashboardSidebarCollapse` button here. -->
+        <UNavigationMenu :items="links" highlight class="-mx-1 flex-1" />
+      </UDashboardToolbar>
+    </template>
+
+    <template #body>
+      <!-- <div
+        :class="
+          !route.path.includes('/settings/roles')
+            ? 'flex flex-col gap-4 sm:gap-6 lg:gap-12 w-full lg:max-w-2xl mx-auto'
+            : 'w-full h-full'
+        "
+      > -->
+      <div class="flex flex-col gap-4 w-full">
+        <NuxtPage />
+      </div>
+    </template>
+  </UDashboardPanel>
+</template>
